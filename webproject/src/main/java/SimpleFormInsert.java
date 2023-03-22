8c8694b720d8ff5c4c238a6407cb0298bdc12a69
Shun <shun4068841@gmail.com>
2023-03-22 10:03:29 -0500

    added all files

---
+++ webproject/src/main/java/SimpleFormInsert.java
@@ -0,0 +1,74 @@
+
+/**
+ * @file SimpleFormInsert.java
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SimpleFormInsert")
+public class SimpleFormInsert extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+   public SimpleFormInsert() {
+      super();
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      String company = request.getParameter("company");
+      String position = request.getParameter("position");
+      String phone = request.getParameter("phone");
+      String desc = request.getParameter("description");
+
+      Connection connection = null;
+      String insertSql = " INSERT INTO job (id, COMPANY, POSITION, COMPPHONE, DESCRIPTION) values (default, ?, ?, ?, ?)";
+
+      try {
+         DBConnection.getDBConnection(getServletContext());
+         connection = DBConnection.connection;
+         PreparedStatement preparedStmt = connection.prepareStatement(insertSql);
+         preparedStmt.setString(1, company);
+         preparedStmt.setString(2, position);
+         preparedStmt.setString(3, phone);
+         preparedStmt.setString(4, desc);
+         preparedStmt.execute();
+         connection.close();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+
+      // Set response content type
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      String title = "Add applied job into DB";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+      out.println(docType + //
+            "<html>\n" + //
+            "<head><title>" + title + "</title></head>\n" + //
+            "<body bgcolor=\"#f0f0f0\">\n" + //
+            "<h2 align=\"center\">" + title + "</h2>\n" + //
+            "<ul>\n" + //
+
+            "  <li><b>Company</b>: " + company + "\n" + //
+            "  <li><b>Position</b>: " + position + "\n" + //
+            "  <li><b>Company Phone number</b>: " + phone + "\n" + //
+            "  <li><b>Description </b>: " + desc + "\n" + //
+
+            "</ul>\n");
+
+      out.println("<a href=/webproject/simpleFormSearch.html>Search Data</a> <br>");
+      out.println("</body></html>");
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
+
+}
